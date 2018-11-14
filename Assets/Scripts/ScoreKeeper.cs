39755b5d6f364e7cf70bb5b3e7a4ee36df29b146
tgrosh <tgrosh@yahoo.com>
2018-11-14 16:38:45 -0500

    Player Score Points

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class ScoreKeeper : MonoBehaviour {
     public int currentScore;
+    public TMPro.TextMeshProUGUI textMesh;
     
     public void AddScore(int amount)
     {
         currentScore += amount;
+        textMesh.text = currentScore.ToString("N0");
     }
 }
